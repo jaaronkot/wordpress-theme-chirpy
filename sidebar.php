741a44979c43e07feb20ee8448a652a72dfdeea5
gezhaoyou <gezhaoyou@126.com>
2023-03-17 23:04:29 +0800

    sidebar 最近更新文章数量为5

--- sidebar.php
+++ sidebar.php
@@ -3,7 +3,7 @@
     <div class="panel-heading">最近更新</div>
     <ul class="post-content pl-0 pb-1 ml-1 mt-2">
         <?php
-            $recent_posts = wp_get_recent_posts();
+            $recent_posts = wp_get_recent_posts(array('numberposts' => 5));
             foreach( $recent_posts as $recent ){
                 echo '<li><a href="' . get_permalink($recent["ID"]) . '">' . $recent["post_title"] . '</a></li>';
             }
