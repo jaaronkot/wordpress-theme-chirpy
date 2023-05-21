5d13e50d98521d4d9c948e029bb5e6a03f81335f
gezhaoyou <gezhaoyou@126.com>
2023-05-21 14:40:25 +0800

    标签云OK

--- templates/module-trending_tags.php
+++ templates/module-trending_tags.php
@@ -1,9 +1,17 @@
-<div id="access-tags">
-        <div class="panel-heading">热门标签</div>
+ <div id="access-tags">
+    <div class="panel-heading">热门标签</div>
         <div class="d-flex flex-wrap mt-3 mb-1 mr-3">
-            <?php if ( function_exists('wp_tag_cloud') ) : ?>
-                <?php wp_tag_cloud('smallest=8&largest=22'); ?>
-            <?php endif; ?>
+            <?php 
+            $count = 0;
+            $tags = get_tags( array(
+                'number' => 10
+            ) );
+            foreach ( $tags as $tag ) {
+                $tag_name = $tag->name;
+                $tag_url = get_tag_link( $tag->term_id );
+                echo '<a class="post-tag" href="' . $tag_url . '">' . $tag_name . '</a>';
+            }
+            ?>
         </div>
     </div>
 </div>
