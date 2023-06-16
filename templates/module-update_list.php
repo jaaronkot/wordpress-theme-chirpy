c99f29bd5a68f38ce1606031ee6c2e9f2fd4321a
gezhaoyou <gezhaoyou@126.com>
2023-06-17 07:23:33 +0800

    跟新细节，添加谷歌统计

--- templates/module-update_list.php
+++ templates/module-update_list.php
@@ -3,11 +3,26 @@
     <div class="panel-heading">最近更新</div>
     <ul class="post-content pl-0 pb-1 ml-1 mt-2">
         <?php
-            $recent_posts = wp_get_recent_posts(array('numberposts' => 5));
-            foreach( $recent_posts as $recent ){
-                echo '<li><a href="' . get_permalink($recent["ID"]) . '">' . $recent["post_title"] . '</a></li>';
+            $args = array(
+                'post_type' => 'post',
+                'posts_per_page' => 5,
+                'orderby' => 'date',
+                'order' => 'DESC'
+            );
+            
+            $query = new WP_Query( $args );
+            
+            if ( $query->have_posts() ) {
+                while ( $query->have_posts() ) {
+                    $query->the_post();
+                    echo '<li><a href="' . get_permalink() . '">' . get_the_title() . '</a></li>';
+                }
+            } else {
+                // 如果没有找到文章，显示一条消息
+                echo '没有找到文章。';
             }
-            wp_reset_query();
+            
+            wp_reset_postdata();
         ?>
     </ul>
 </div> <!-- #access-lastmod -->
