bae0a45a44b523a6bb3f564263664dfc8b5653fa
gezhaoyou <gezhaoyou@126.com>
2023-06-17 14:22:37 +0800

    重构归档页面和xue主题一致

--- templates/post-arch.php
+++ templates/post-arch.php
@@ -12,34 +12,32 @@
     foreach ($years as $year) {
       echo '<div class="year lead">' . $year . '</div>';
       echo '<ul class="list-unstyled">';
-    
-      $args = array(
-        'post_type' => 'post',
-        'posts_per_page' => -1, // 获取所有文章
-        'date_query' => array(
-            array(
-                'year' => $year,
-            ),
-        ),
-    );
-    
-    $query = new WP_Query( $args );
-    
-    if ( $query->have_posts() ) {
-        while ( $query->have_posts() ) {
-            $query->the_post();
-            echo '<li>';
-            echo '  <span class="date month small text-muted ml-1" >' . '- ' . get_the_date('m') . '-' . get_the_date('d') . '</span>';
-            echo '  <a href="' . get_the_permalink() . '">' . get_the_title() . '</a>';
-            echo '</li>';
-        }
-    } else {
-        // 如果没有找到文章
-        echo '没有找到符合条件的文章';
-    }
-    // 重置查询
-    wp_reset_postdata();
-    echo '</ul>';
+      for ($month = 1; $month <= 12; $month++) {
+        $args = array(
+            'post_type' => 'post',
+            'posts_per_page' => -1, // 获取所有文章
+            'year' => $year,
+            'monthnum' => $month,
+            );
+        
+            $query = new WP_Query( $args );
+            
+            if ( $query->have_posts() ) {
+                echo '<span class="title-month small text-muted ml-1" >' . $month . ' 月 </span>';
+                while ( $query->have_posts() ) {
+                    $query->the_post();
+                    echo '<li>';
+                    echo '  <span class="date month small text-muted ml-1" >' . get_the_date('m') . '-' . get_the_date('d') . '</span>';
+                    echo '  <a href="' . get_the_permalink() . '">' . get_the_title() . '</a>';
+                    echo '</li>';
+                }
+            } else {
+                continue;
+            }
+            // 重置查询
+            wp_reset_postdata();
+            echo '</ul>';
+      }
     }
 ?>
 </div>
